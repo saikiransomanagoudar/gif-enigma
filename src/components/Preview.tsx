6f43d36eb2096544e39bc8cff2ba5272144d0303
saikiransomanagoudar <saikiransomanagoudar@gmail.com>
2025-10-12 23:32:33 -0500

    refactor: background UI color change and the loading gif change
    Signed-off-by: saikiransomanagoudar <saikiransomanagoudar@gmail.com>

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -3,9 +3,9 @@ import { Devvit } from '@devvit/public-api';
 export const Preview: Devvit.BlockComponent = () => {
   return (
     <zstack width={'100%'} height={'100%'} alignment="center middle">
-      <vstack width={'100%'} height={'100%'} alignment="center middle">
+      <vstack width={'100%'} height={'100%'} alignment="center middle" darkBackgroundColor="#0d1629" lightBackgroundColor="#E8E5DA" cornerRadius="medium">
         <image
-          url="loading.gif"
+          url="eyebrows.gif"
           height={'140px'}
           width={'140px'}
           imageHeight={'240px'}
